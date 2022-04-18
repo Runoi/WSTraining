fa2af92926fecb0b988ea1f9745e425ca8173ca0
Runoi <75607022+Runoi@users.noreply.github.com>
2022-04-18 19:14:59 +0300

    BD work!

--- WSTraining/MainWindow.xaml.cs
+++ WSTraining/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 
 namespace WSTraining
 {
@@ -20,9 +12,20 @@ namespace WSTraining
     /// </summary>
     public partial class MainWindow : Window
     {
+        OrderContext db;
         public MainWindow()
         {
             InitializeComponent();
+            db = new OrderContext();
+            db.Orders.Load();
+            Order.ItemsSource = db.Orders.Local.ToBindingList();
+
+
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
--- WSTraining/Model/Model.cs
+++ WSTraining/Model/Model.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 
-namespace WSTraining.Model
+namespace WSTraining
 {
     internal class Model
     {
@@ -14,39 +16,102 @@ namespace WSTraining.Model
 
     public class Order_Prod
     {
+        [Key]
         public int Order_Id { get; set; }
         public int Product_Id { get; set; }
         public int Count { get; set; }
+
+        public Order_Prod OrderProd { get; set; }
 
     }
 
     public class Order
     {
-        public int Id { get; set; }
+        [Key]
+        public int Order_Id { get; set; }
+        
         public int User_Id { get; set; }
         public DateTime Date { get; set; }
+
+        public ICollection<Order_Prod> Orders { get; set; }
+
+        public Order()
+        {
+            Orders = new List<Order_Prod>();
+        }
+
 
     }
     
     public class User
     {
-        public int Id { get; set; }
+        [Key]
+        public int User_Id { get; set; }
+        
+        
         public string Email { get; set; }
         public string Password { get; set; }
         public string Full_Name { get; set; }
         public int age { get; set; }
         public string Phone_Number { get; set; }
 
+        public ICollection<Order> Orders { get; set; }
+
+        public User()
+        {
+            Orders = new List<Order>();
+        }
+
+
+    }
+
+    public class Product
+    {
+        [Key]
+        public int Product_Id { get; set; }
+        public string Name { get; set; }
+        
+        public int Catrgory_Id { get; set; }
+        public int Price { get; set; }
+        public bool Available { get; set; }
+
+        public ICollection<Order_Prod> Products { get; set; }
+
+        public Product()
+        {
+            Products = new List<Order_Prod>();
+        }
+
+
+    }
+
+    public class Category
+    {
+        [Key]
+        public int Category_Id { get; set; }
+        public string Category_Name { get; set; }
+
+        public ICollection<Product> Categorys { get; set; }
+
+        public Category()
+        {
+            Categorys = new List<Product>();
+        }
+
     }
     
-    public class MobileContext : DbContext
+    public class OrderContext : DbContext
     {
-        public MobileContext() : base("DefaultConnection")
+        public OrderContext() : base("DefaultConnection")
         {
           
         }
 
         public DbSet<User> Users { get; set; }
-        
+        public DbSet<Order_Prod> Order_Prods { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categorys { get; set; }
+
     }
 }
